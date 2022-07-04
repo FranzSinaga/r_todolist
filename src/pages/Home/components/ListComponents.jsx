b7262f5ffa4bcf1bb5190b7f06fc734144c334c1
Franz Aditya Natanael Sinaga <sinagafranz12@gmail.com>
2022-07-04 16:16:19 +0700

    edit function in task item

--- src/pages/Home/components/ListComponents.jsx
+++ src/pages/Home/components/ListComponents.jsx
@@ -1,8 +1,11 @@
+import React, { useState } from "react";
+
 export const ListItem = (props) => {
   const value = props.value;
   const allDataTask = JSON.parse(localStorage.getItem('todos'));
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [itemValue, setItemvalue] = useState(value)
   const deleteTask = (value) => {
-    console.log('delete ', value)
     const delTask = allDataTask.filter(e => {
       return e !== value
     })
@@ -10,10 +13,43 @@ export const ListItem = (props) => {
     window.location.reload(); // reload untuk sementara
   }
 
+  const editTask = () => {
+    let temp = [...allDataTask];
+    const idx = allDataTask.indexOf(value)
+    console.log(idx)
+    temp[idx] = itemValue;
+    console.log(temp)
+    localStorage.setItem("todos", JSON.stringify(temp));
+    setIsDisabled(true)
+    window.location.reload();
+  }
+
   return (
     <li>
-      {value} &nbsp;
-      <button type="button" onClick={() => deleteTask(value)}>Del</button>
+      <input
+        type="text"
+        name="newTask"
+        value={itemValue}
+        autoComplete="off"
+        disabled={isDisabled}
+        onChange={(e) => setItemvalue(e.target.value)}
+      ></input>
+      {isDisabled
+        ? null
+        : <button type="button" onClick={() => editTask()}>Confirm</button>
+      }
+      <button
+        type="button"
+        onClick={() => {
+          setIsDisabled(!isDisabled);
+          setItemvalue(value);
+        }}>
+        {isDisabled ? 'Edit' : 'Cancel'}
+      </button>
+      {isDisabled
+        ? <button type="button" onClick={() => deleteTask(value)}>Delete</button>
+        : null
+      }
     </li>
   );
 }
